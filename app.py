2148431c4a6a36549104bbc290380656f0f578d4
adeeb20241760 <adeeb.20241760@iit.ac.lk>
2026-04-18 11:37:50 +0530

    Imported dataset

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 st.title("Hello, Streamlit!")
 st.write("This is a simple Streamlit app.")
+import pandas as pd
+trade_data_lk = pd.read_excel('trade_lka.xlsx')
+print(trade_data_lk)
+st.line_chart(trade_data_lk['Value'])
